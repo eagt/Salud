789c0092c99512d4e3a742676ec8e4f24a13c14a
geo031980 <geo031980@yahoo.com>
2016-04-04 20:07:21 -0500

    bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
   #ßinclude SessionsHelper
-
   #around_filter :user_time_zone, if: :current_user
 
   helper_method :current_user
@@ -13,13 +11,9 @@ class ApplicationController < ActionController::Base
   def current_user
     # Take the URL to extract the resource: [Professional or Company]
     resource= request.path.split('/')[1]
-    
-    @is_company = resource == "companies" ? true : false
-        
-    if resource && params[resource.singularize+"_id"]
-      @current_user = resource.singularize.classify.constantize.find(params[resource.singularize+"_id"])
-    end
-  end
-
-
+      @is_company = resource == "companies" ? true : false
+      if resource && params[resource.singularize+"_id"]
+        @current_user = resource.singularize.classify.constantize.find(params[resource.singularize+"_id"])
+        end
+      end
 end
--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,45 +1,44 @@
 class AppointmentsController < ApplicationController
   before_action :set_appointment, only: [:show, :edit, :update, :destroy]
 
-  # GET /appointments
-  # GET /appointments.json
   def index
-    @clinic = Clinic.find(params[:clinic_id])
-    @appointments = @clinic.appointments
+    @appointments = Appointment.all
+    #@clinic = Clinic.find(params[:clinic_id])
+    #@appointments = @clinic.appointments.all
   end
 
-  # GET /appointments/1
-  # GET /appointments/1.json
   def show
     @appointment = Appointment.find(params[:id])
   end
 
-  # GET /appointments/new
   def new
     @clinic = Clinic.find(params[:clinic_id])
     @appointment = @clinic.appointments.build
   end
 
-  # POST /appointments
-  # POST /appointments.json
   def create
-    @clinic = Clinic.find(params[:clinic_id])
-    @appointment = @clinic.appointments.build(appointment_params)
+      @clinic = Clinic.find(params[:clinic_id])
+      @appointment = @clinic.appointments.create(appointment_params)
       if @appointment.save
-        flash[:notice] = " Successfully created Appointment."
-        redirect_to(clinic_appointments_url)
-      else
-        render action: :new 
-      end
-  end
+         flash[:notice] = " Successfully created Appointment."
+         redirect_to clinic_path(@appointment)
+       end
+    end
 
-   # GET /appointments/1/edit
+  # def create
+  #   @clinic = Clinic.find(params[:clinic_id])
+  #   @appointment = @clinic.appointments.build(appointment_params)
+  #     if @appointment.save
+  #       flash[:notice] = " Successfully created Appointment."
+  #       redirect_to(clinic_appointments_url)
+  #     else
+  #       render action: :new
+  #     end
+  # end
+
   def edit
   end
 
-
-  # PATCH/PUT /appointments/1
-  # PATCH/PUT /appointments/1.json
   def update
     respond_to do |format|
       if @appointment.update(appointment_params)
@@ -52,23 +51,19 @@ class AppointmentsController < ApplicationController
     end
   end
 
-  # DELETE /appointments/1
-  # DELETE /appointments/1.json
   def destroy
     @appointment.destroy
     respond_to do |format|
-      format.html { redirect_to appointments_url, notice: 'Appointment was successfully destroyed.' }
+      format.html { redirect_to @appointments, notice: 'Appointment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_appointment
       @appointment = Appointment.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def appointment_params
       params.require(:appointment).permit(:clinic_id, :comment, :date, :creator)
     end
